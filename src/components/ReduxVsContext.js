c2a8db5dd6f06e45dc18dbe6ab82f6c00dc6f80d
Colson <colsoncalifornication@gmail.com>
2024-04-17 23:37:37 +0545

    redux vs context documentation

--- src/components/ReduxVsContext.js
+++ src/components/ReduxVsContext.js
@@ -83,3 +83,55 @@
  * The LanguageStore is just about recording what the currently selected
  * language is and giving the ability to change it.
  * */
+
+/* @ REDUX vs CONTEXT in NUTSHELL!!! */
+
+/* @ WHY WOULD WE USE REDUX? */
+/* With Redux, we get some excellet documentation. It is pretty top notch.
+ * There are great examples. We get some excellent documentation.
+ * In addition with Redux, there are extremely well-known design patterns.
+ * Online there are dozens, hundreds, thousands of different tutorials that
+ * tell us exactly how to work with Redux and how to handle a variety of
+ * different scenarios.
+ * In addition, there are a tremendous number of open source libraries specifically
+ * made to help us work with Redux.
+ * So these are all very good reasons to contrinue to use REDUX inside of any
+ * serious project you plan to work on.
+ */
+
+/* @ WHY WOULD WE USE CONTEXT? */
+/* With Context, we don't have to use an extra library, and that's kind of like
+ * pretty much it to be honest.
+ * That's the only big benefit to using Context in place of Redux.
+ * We do not have to use a separate library.
+ * Now, some of the challenges of Context are that it's really hard to build
+ * one of these store components like the lanaguage store that we just saw that
+ * has cross-cutting concerns.
+ * In other words, we would probably want to build a separate store component
+ * for each type of resource that we have inside of our application.
+ * So for example, if we have languages, we would have LanguageStore.
+ * If we plan to have authentication, we might want to create a UserStore or
+ * something like that.
+ * And these would probably all be separate React components.
+ * Sharing data between these different stores would be really challenging.
+ * But when using Redux, inside of a single action creator, we are able to
+ * reach into our entire state object with getState, and get access to all the
+ * data inside of Redux i.e data of our entire application and use it.
+ * `export const createStream = formValues => async (dispatch, getState) => {}`
+ * Doing that same thing inside of one of these store components would be far
+ * far more challenging.
+ * We would have to make sure that we called any callback with all the appropriate
+ * arguments. So essentially inside of some component, if we wanted to associate
+ * a language with, say, like a user ID for some crazy reason, we would have to
+ * call this callback function with the language and the user id.
+ * `onLanguageChange = language => this.setState({ language })`
+ * It would be really hard to write some code inside of here that reach into
+ * another store of data and pull some data out of it.
+ * So that's definately a really big challenge with this approach.
+ *
+ * So there are good reasons to use Context. There's a little bit less code
+ * that we would have to write. We don't have to use that extray library i.e Redux.
+ */
+
+/* @ At the end of the day, personally it makes a lot of sense to continue to
+ * use Redux for any serious application that you plan to work on. */
