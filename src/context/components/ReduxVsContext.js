7c1ddaade33171e35d2f339a9d11afc1cca5981a
Colson <colsoncalifornication@gmail.com>
2024-04-17 16:01:41 +0545

    architect project structure for refactor

---
+++ src/context/components/ReduxVsContext.js
@@ -0,0 +1,17 @@
+/* @ Redux */
+/*
+ * Redux through the Redux Library and react-redux together provides a way
+ * to distribute data to various components throughout our component hierarchy.
+ * It gives us a wayto centralize all of our data inside of a common store.
+ * And it also provides some means of changing data inside that store by
+ * dispatching action creators, reducers and all kind of stuff).
+ */
+
+/* @ Context */
+/*
+ * With context system by itself, all we really have is a way to distribute data
+ * to various components inside of our application i.e A way to communicate from
+ * a parent component down to a child.
+ */
+
+/* Context system is far more limited than Redux is! */
---
+++ src/redux/components/UserCreate.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Field from './Field';
+import Button from './ButtonConsumer';
+
+const UserCreate = () => {
+  return (
+    <div className='ui form'>
+      <Field />
+      <Button />
+    </div>
+  );
+};
+
+export default UserCreate;
