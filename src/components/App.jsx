f969c261dc9774a7c705bb6dbbe5883a20019293
Colson <colsoncalifornication@gmail.com>
2024-04-17 14:15:35 +0545

    create provider component: communicate some information from app component into the context object

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
 
 /* Context
  * Two ways to get information in to the context object: (Source of data)
@@ -32,6 +33,29 @@ class App extends React.Component {
             onClick={() => this.onLanguageChange('nepali')}
           />
         </div>
+        {/* Communication some information from our App Component to the 
+        Context Object */}
+
+        {/* For context value, usually we will have a value prop coming from 
+        props object or a state object or something similar because we usually 
+        want to change the value inside of our context object over time */}
+
+        {/* NOTE: Each separate use of <LanguageContext.Provider> creates a new
+        separate 'pipe' of information! Or essentially kind of a new pipe that 
+        conveys information from that parent component down to those remote children*/}
+
+        {/* Only this first one sees that update */}
+        <LanguageContext.Provider value={this.state.language}>
+          <UserCreate />
+        </LanguageContext.Provider>
+
+        {/* This second one gets its value from a fixed provider that always has `nepali`  */}
+        <LanguageContext.Provider value='nepali'>
+          <UserCreate />
+        </LanguageContext.Provider>
+
+        {/* The last one gets its value from the default value from that context object
+        that we set to english */}
         <UserCreate />
       </div>
     );
